35cff1160c414c05312216f6b547e95fe6494540
munroe1786 <era3munroe@gmail.com>
2021-04-05 21:42:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function findMinAndRemove(array){
-
+    let minimum = array[0];
+    let minimumIndex = 0;
+    for (let i = 0; i < array.length; i++) {
+        let currentElement = array[i];
+        if (array[i] < minimum) {
+            minimum = array[i];
+            minimumIndex = i;
+        }
+    }
+    array.splice(minimumIndex, 1);
+    return minimum;
 }
 
 function selectionSort(array){
-
+    let newMinimum;
+    let sortedArray = [];
+    while (array.length != 0) {
+        newMinimum = findMinAndRemove(array);
+        sortedArray.push(newMinimum);
+    }
+    return sortedArray;
 }
